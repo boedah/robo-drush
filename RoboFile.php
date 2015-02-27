b4eb4cde2e56b7f61b07cc5ef3abbdaa7db25025
Peter Gasser <github@boedah.de>
2015-02-27 23:27:52 +0100

    use `option` method for PHPUnit options

--- RoboFile.php
+++ RoboFile.php
@@ -8,11 +8,11 @@ class RoboFile extends \Robo\Tasks
     {
         $this->stopOnFail(true);
         $this->taskPHPUnit()
-            ->arg('-v')
-            ->arg('-d error_reporting=-1')
             ->option('disallow-test-output')
             ->option('report-useless-tests')
             ->option('strict-coverage')
+            ->option('-v')
+            ->option('-d error_reporting=-1')
             ->arg('tests')
             ->run();
     }
