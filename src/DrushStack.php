786b5f8abdfbb2b418c056d19cb62cdd41f5b41f
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2017-08-21 10:06:33 +0200

    Issue #5: Added support for  Drush 9.

--- src/DrushStack.php
+++ src/DrushStack.php
@@ -318,6 +318,9 @@ class DrushStack extends CommandStack
             $isPrinted = $this->isPrinted;
             $this->isPrinted = false;
             $result = $this->executeCommand($this->executable . ' --version');
+            if (!$result->wasSuccessful()) {
+              $result = $this->executeCommand($this->executable . ' version');
+            }
             $output = $result->getMessage();
             $this->drushVersion = 'unknown';
             if (preg_match('#[0-9.]+#', $output, $matches)) {
